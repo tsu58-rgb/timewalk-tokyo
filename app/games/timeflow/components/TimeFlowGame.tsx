76fb2df94e227650fcf1ff040b59d060833063de
tsu58-rgb <t.nakamatsu58@gmail.com>
2026-06-06 09:13:02 +0900

    Hide TimeFlow reorder buttons after answer check

--- app/games/timeflow/components/TimeFlowGame.tsx
+++ app/games/timeflow/components/TimeFlowGame.tsx
@@ -326,7 +326,7 @@ export default function TimeFlowGame() {
                         </span>
                       )}
 
-                      {!result?.correct && (
+                      {!result && (
                         <>
                           <button
                             type="button"
