994e406f0466a576c39e256ae712024366164548
tsu58-rgb <t.nakamatsu58@gmail.com>
2026-06-10 11:56:07 +0900

    Add spot quiz sheet URL

--- app/lib/sheetUrls.ts
+++ app/lib/sheetUrls.ts
@@ -1,8 +1,10 @@
-export const EVENTS_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs_sHwnzRP6UbWvwqiCURTbMWS8yrFRRErdzLk_Xt3w1vvBhS6Wa3nO7MulssNWSQ80aqlgM5B2x4Y/pub?gid=1015785763&single=true&output=csv";
+const SHEETS_BASE_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs_sHwnzRP6UbWvwqiCURTbMWS8yrFRRErdzLk_Xt3w1vvBhS6Wa3nO7MulssNWSQ80aqlgM5B2x4Y/pub";
 
-export const SPOTS_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs_sHwnzRP6UbWvwqiCURTbMWS8yrFRRErdzLk_Xt3w1vvBhS6Wa3nO7MulssNWSQ80aqlgM5B2x4Y/pub?gid=1242477641&single=true&output=csv";
+export const EVENTS_URL = `${SHEETS_BASE_URL}?gid=1015785763&single=true&output=csv`;
 
-export const CHARACTERS_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs_sHwnzRP6UbWvwqiCURTbMWS8yrFRRErdzLk_Xt3w1vvBhS6Wa3nO7MulssNWSQ80aqlgM5B2x4Y/pub?gid=1745190060&single=true&output=csv";
+export const SPOTS_URL = `${SHEETS_BASE_URL}?gid=1242477641&single=true&output=csv`;
+
+export const CHARACTERS_URL = `${SHEETS_BASE_URL}?gid=1745190060&single=true&output=csv`;
+
+export const SPOT_QUIZZES_URL = `${SHEETS_BASE_URL}?gid=987654321&single=true&output=csv`;
