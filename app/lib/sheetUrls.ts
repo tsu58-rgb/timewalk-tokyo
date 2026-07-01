99cb952694197633229c905516eca368647fd1ea
tsu58-rgb <t.nakamatsu58@gmail.com>
2026-07-01 13:50:43 +0900

    Add works sheet URL

--- app/lib/sheetUrls.ts
+++ app/lib/sheetUrls.ts
@@ -2,13 +2,9 @@ const SHEETS_BASE_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs_sHwnzRP6UbWvwqiCURTbMWS8yrFRRErdzLk_Xt3w1vvBhS6Wa3nO7MulssNWSQ80aqlgM5B2x4Y/pub";
 
 export const EVENTS_URL = `${SHEETS_BASE_URL}?gid=1015785763&single=true&output=csv`;
-
 export const SPOTS_URL = `${SHEETS_BASE_URL}?gid=1242477641&single=true&output=csv`;
-
+export const WORKS_URL = `${SHEETS_BASE_URL}?gid=697753165&single=true&output=csv`;
 export const CHARACTERS_URL = `${SHEETS_BASE_URL}?gid=1745190060&single=true&output=csv`;
-
 export const SPOT_QUIZZES_URL = `${SHEETS_BASE_URL}?gid=987654321&single=true&output=csv`;
-
 export const COURSES_URL = `${SHEETS_BASE_URL}?gid=21001&single=true&output=csv`;
-
 export const COURSE_POINTS_URL = `${SHEETS_BASE_URL}?gid=21002&single=true&output=csv`;
